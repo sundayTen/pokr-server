5336974e940f220bcf71a1d5f7070b070669409a
hojunin <dlsghwns@naver.com>
2022-11-13 15:57:22 +0900

    feat: 테일윈드 설정

--- pokr-web/app/layout.tsx
+++ pokr-web/app/layout.tsx
@@ -1,18 +1,21 @@
-import './globals.css'
+import './globals.css';
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+	return (
+		<html lang="kn">
+			{/* head 컴포넌트 아래에, 각 페이지별 head 컴포넌트에 적힌 내용은 이 컴포넌트의 내용을 상속함. */}
+			<head>
+				{/* 
+        viewport 관련 기본 설정이 포함되어야 함
+        링크 : https://developer.mozilla.org/en-US/docs/Web/HTML/Viewport_meta_tag
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 }
---
+++ pokr-web/app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+	return (
+		<div>
+			<h1>페이지</h1>
+		</div>
+	);
+};
+
+export default Page;
