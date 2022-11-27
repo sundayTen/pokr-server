364861cd21b1922ac2c9c5e3893fa8e133d3a18f
hojunin <dlsghwns@naver.com>
2022-11-27 11:18:57 +0900

    feat: prettier 설정 조정 - tabWidth
    feat: lint 설정 변경
    
    feat: test

--- pokr-web/app/test/page.tsx
+++ pokr-web/app/test/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Page = () => {
-	return (
-		<div>
-			<h1>페이지</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>페이지</h1>
+    </div>
+  );
 };
 
 export default Page;
