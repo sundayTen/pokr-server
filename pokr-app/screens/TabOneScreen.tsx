2a43d14d971065660c185a1bb6c5d31893318ed8
hojunin <dlsghwns@naver.com>
2023-01-24 19:11:25 +0900

    feat: webview 세팅

--- pokr-app/screens/TabOneScreen.tsx
+++ pokr-app/screens/TabOneScreen.tsx
@@ -1,32 +1,27 @@
+import React from 'react';
 import { StyleSheet } from 'react-native';
+import WebView from 'react-native-webview';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { HOST } from '../utils/webview';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+export default function TabOneScreen({
+	navigation,
+}: RootTabScreenProps<'TabOne'>) {
+	return (
+		<View style={styles.container}>
+			<WebView
+				source={{
+					uri: HOST,
+				}}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+	container: {
+		flex: 1,
+	},
 });
