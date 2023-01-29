d22ab13506b011f70678936697fb4feec2bfdefe
tagrn <fksk94@gmail.com>
2023-01-29 15:00:23 +0900

    init: separated into three repositories - app, web, server

--- pokr-web/app/goal-management/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const GoalManagement = () => {
-  return <div>goal-management</div>;
-};
-
-export default GoalManagement;
--- pokr-web/app/memoir/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Memoir = () => {
-  return <div>Memoir</div>;
-};
-
-export default Memoir;
