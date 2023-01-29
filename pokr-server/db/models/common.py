c0478a8b92344aa0a42512d2cf8b0763dd97b1b1
tagrn <fksk94@gmail.com>
2023-01-29 10:37:06 +0900

    feat: commoon date model

---
+++ pokr-server/db/models/common.py
@@ -0,0 +1,15 @@
+from sqlalchemy import DateTime, Column, func
+
+from db.config import Base
+
+
+class DateBase(Base):
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
