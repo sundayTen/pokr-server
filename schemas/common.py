9570bbb195c39502572f245a553d29ba4ac73244
tagrn <fksk94@gmail.com>
2023-04-07 17:00:15 +0900

    feat: calculating metrics logic for dashboard

--- schemas/common.py
+++ schemas/common.py
@@ -1,6 +1,13 @@
+from enum import Enum, auto
+
 from pydantic import BaseModel
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class PeriodCategory(Enum):
+    QUARTER = auto()
+    MONTH = auto()
--- service/metrics.py
+++ service/metrics.py
@@ -1,8 +1,13 @@
+import time
 from datetime import datetime
 from functools import reduce
 from itertools import chain
+from typing import List
 
-from env import OBJECTIVE_WEIGHT
+from env import OBJECTIVE_WEIGHT, KEY_RESULT_WEIGHT, KEY_RESULT_MAX_SCORE
+from schemas.common import PeriodCategory
+from schemas.initiative import InitiativeSchema
+from schemas.key_result import KeyResultWithInitiativesSchema
 from schemas.metrics import ObjectiveAchievementPercentSchema
 from schemas.objective import ObjectiveWithKeyResultsSchema
 
@@ -35,16 +40,66 @@ async def get_objective_achievement_percent(
     )
 
 
-async def calculate_achievement_percent(objective: ObjectiveWithKeyResultsSchema):
-    achievement_point, total_point = get_objective_achievement_percent(objective)
+async def calculate_achievement_percent(
+    objective: ObjectiveWithKeyResultsSchema, category: PeriodCategory
+):
     key_results = objective.key_results
     initiatives = list(chain(*[key_result.initiatives for key_result in key_results]))
-    # TODO 달성도 로직 만드는 중
+    points_for_calculating_percent = [
+        await get_objective_achievement_percent(objective, category),
+        await get_key_results_achievement_percent(key_results, category),
+        await get_initiatives_achievement_percent(initiatives, category),
+    ]
+
+    return int(
+        sum([point[0] for point in points_for_calculating_percent])
+        / sum([point[1] for point in points_for_calculating_percent])
+        * 100
+    )
 
 
-async def get_objective_achievement_percent(objective: ObjectiveWithKeyResultsSchema):
-    if datetime.now().year - 1 == objective.year:
-        if objective.achievement:
+async def get_objective_achievement_percent(
+    objective: ObjectiveWithKeyResultsSchema, category: PeriodCategory
+) -> (int, int):
+    now = datetime.now()
+    if now.year - 1 != objective.year:
+        return 0, 0
+
+    if category == PeriodCategory.QUARTER:
+        if now.month == 1 and now.day < 8 and objective.achievement:
             return OBJECTIVE_WEIGHT, OBJECTIVE_WEIGHT
-        return 0, OBJECTIVE_WEIGHT
-    return 0, 0
+    elif category == PeriodCategory.MONTH:
+        if now.month == 1 and objective.achievement:
+            return OBJECTIVE_WEIGHT, OBJECTIVE_WEIGHT
+
+    return 0, OBJECTIVE_WEIGHT
+
+
+async def get_key_results_achievement_percent(
+    key_results: List[KeyResultWithInitiativesSchema], category: PeriodCategory
+) -> (float, int):
+    achievement_scores = []
+    now = datetime.now()
+    if category == PeriodCategory.QUARTER:
+        current_time = time.mktime(now.date().timetuple())
+        for key_result in key_results:
+            due_time = time.mktime(key_result.due_date.timetuple())
+            if due_time < current_time < due_time + 86400 * 8:
+                achievement_scores.append(key_result.achievement_score)
+    elif category == PeriodCategory.MONTH:
+        current_month = now.year * 12 + now.month
+        for key_result in key_results:
+            due_month = key_result.due_date.year * 12 + key_result.due_date.month
+            if due_month + 1 == current_month:
+                achievement_scores.append(key_result.achievement_score)
+
+    return (
+        sum(achievement_scores) / KEY_RESULT_MAX_SCORE * KEY_RESULT_WEIGHT,
+        len(achievement_scores) * KEY_RESULT_WEIGHT,
+    )
+
+
+async def get_initiatives_achievement_percent(
+    initiatives: List[InitiativeSchema], category: PeriodCategory
+) -> (float, int):
+    pass
