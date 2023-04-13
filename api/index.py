c4d4f8c9b73e09d1516b5b034b52ff168f17df28
tagrn <fksk94@gmail.com>
2023-04-13 14:46:27 +0900

    chore: remove dummy user api

--- api/index.py
+++ api/index.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from db.models.user import User
 from env import ADMIN_USER, JWT_PREFIX
 from errors import CREDENTIALS_EXCEPTION
-from jwt import create_access_token
 from schemas.common import Token
 
 router = APIRouter()
@@ -21,9 +19,3 @@ async def login_for_access_token(data: OAuth2PasswordRequestForm = Depends()) ->
         raise CREDENTIALS_EXCEPTION
 
     return Token(access_token=data.password, token_type=JWT_PREFIX)
-
-
-@router.get("/get-dummy-user", description="JWT 더미 유저 생성")
-async def get_dummy_user() -> Token:
-    access_token: str = await create_access_token(User(id=1), 365)
-    return Token(access_token=access_token, token_type=JWT_PREFIX)
