fb149965362c39e7d02f1032e0cd81a68bdbfc33
tagrn <fksk94@gmail.com>
2023-04-15 12:05:39 +0900

    refactor: create new user logic

--- api/auth.py
+++ api/auth.py
@@ -1,13 +1,13 @@
 import json
+import logging
 import urllib.request
 
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from crud.nickname.read import get_random_nickname
+from crud.user.create import create_new_user
 from crud.user.read import get_user_by_platform_and_sns_key
 from db.config import get_db
-from db.models.user import User
 from env import (
     JWT_PREFIX,
     NAVER_PROFILE_URL,
@@ -38,22 +38,13 @@ async def signup(
         response_body = response.read()
         sns_key = json.loads(response_body.decode("utf-8"))["response"]["id"]
     else:
+        logging.info(response)
         raise CREDENTIALS_EXCEPTION
 
     encrypted_sns_key = await encrypt_data(sns_key)
     user = await get_user_by_platform_and_sns_key(Platform.NAVER, encrypted_sns_key)
-    # TODO 리팩토링 필요
     if not user:
-        nickname = await get_random_nickname(db)
-        user = User(
-            platform=Platform.NAVER.name,
-            sns_key=encrypted_sns_key,
-            nickname=nickname,
-        )
-        nickname.count += 1
-        db.add(nickname)
-        db.add(user)
-        db.commit()
+        create_new_user(Platform.NAVER.name, encrypted_sns_key, db)
 
     return TokenResponse(
         access_token=await create_access_token(user), token_type=JWT_PREFIX
---
+++ crud/user/create.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import Session
+
+from crud.nickname.read import get_random_nickname
+from db.models.user import User
+from schemas.common import Platform
+
+
+async def create_new_user(
+    platform: Platform, encrypted_sns_key: str, db: Session
+) -> User:
+    nickname = await get_random_nickname(db)
+    user = User(
+        platform=platform, sns_key=encrypted_sns_key, nickname=nickname.nickname
+    )
+    nickname.count += 1
+    db.add(nickname)
+    db.add(user)
+    db.commit()
