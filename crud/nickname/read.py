d3877c83cd7d2bb5fcc3e44d508470b3070bb1f5
tagrn <fksk94@gmail.com>
2023-07-08 15:11:32 -0700

    fix: duplicated random nickname

--- crud/nickname/read.py
+++ crud/nickname/read.py
@@ -7,7 +7,7 @@ from db.models.nickname import Nickname
 
 async def get_random_nickname(db: Session):
     latest_nickname = db.query(Nickname).order_by(Nickname.id.desc()).first()
-    random_number = (random() * 10000 % latest_nickname.id) + 1
+    random_number = (random() * (10**9) % latest_nickname.id) + 1
     nickname = (
         db.query(Nickname)
         .filter(Nickname.id >= random_number)
--- crud/user/create.py
+++ crud/user/create.py
@@ -10,7 +10,10 @@ async def create_new_user(
 ) -> User:
     nickname = await get_random_nickname(db)
     user = User(
-        platform=platform, sns_key=encrypted_sns_key, nickname=nickname.nickname
+        platform=platform,
+        sns_key=encrypted_sns_key,
+        nickname=nickname.nickname
+        + (" " + str(nickname.count) if nickname.count else ""),
     )
     nickname.count += 1
     db.add(nickname)
